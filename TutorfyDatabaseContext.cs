9a44342df84ca55ac5a2e3509a7a3882fb5c8056
Chris <chrispackett1998@gmail.com>
2018-09-22 16:25:42 -0400

    created dbcontext

---
+++ TutorfyDatabaseContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace tutorfy-backend
+{
+    public partial class TutorfyDatabaseContext : DbContext
+    {
+        public TutorfyDatabaseContext()
+        {
+        }
+
+        public TutorfyDatabaseContext(DbContextOptions<TutorfyDatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseNpgsql("server=localhost;database=TutorfyDatabase");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {}
+    }
+}
