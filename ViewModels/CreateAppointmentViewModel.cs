b4b99d66170f4e33d07fdbd68ea02f5c14bccc52
Chris <chrispackett1998@gmail.com>
2018-09-25 13:26:50 -0400

    changed CreateAppointmentViewModel AppointmentLength to double

--- ViewModels/CreateAppointmentViewModel.cs
+++ ViewModels/CreateAppointmentViewModel.cs
@@ -1,4 +1,3 @@
-
 using System;
 
 namespace tutorfy_backend.ViewModels
@@ -6,7 +5,7 @@ namespace tutorfy_backend.ViewModels
     public class CreateAppointmentViewModel
     {
         public DateTime StartTime { get; set; }
-        public int AppointmentLength { get; set; }
+        public double AppointmentLength { get; set; }
         public string Location { get; set; }
     }
 }
