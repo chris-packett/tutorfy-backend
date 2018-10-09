f6e3a47830d72b867da1afb46b8507e62f154a54
Chris <chrispackett1998@gmail.com>
2018-10-09 16:35:27 -0400

    updated quizzes table to have only 3 answers

---
+++ Migrations/20181009203337_RemovedIsTutorIsStudentAndAnswerFourAndFiveFromQuizTable.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace tutorfybackend.Migrations
+{
+    public partial class RemovedIsTutorIsStudentAndAnswerFourAndFiveFromQuizTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AnswerFive",
+                table: "Quizzes");
+
+            migrationBuilder.DropColumn(
+                name: "AnswerFour",
+                table: "Quizzes");
+
+            migrationBuilder.DropColumn(
+                name: "IsStudent",
+                table: "Quizzes");
+
+            migrationBuilder.DropColumn(
+                name: "IsTutor",
+                table: "Quizzes");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AnswerFive",
+                table: "Quizzes",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "AnswerFour",
+                table: "Quizzes",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsStudent",
+                table: "Quizzes",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsTutor",
+                table: "Quizzes",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
--- Models/Quiz.cs
+++ Models/Quiz.cs
@@ -5,13 +5,9 @@ namespace tutorfy_backend.Models
     public class Quiz
     {
         public int Id { get; set; }
-        public bool IsStudent { get; set; }
-        public bool IsTutor { get; set; }
         public int AnswerOne { get; set; }
         public int AnswerTwo { get; set; }
         public int AnswerThree { get; set; }
-        public int AnswerFour { get; set; }
-        public int AnswerFive { get; set; }
         public bool isCompleted { get; set; }
     }
 }
